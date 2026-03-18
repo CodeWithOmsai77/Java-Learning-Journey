9b9b6de2fd56649a71b68226a2ad6a1039411f09
Omsai Ganesh Dasari <omsai.freelancer7@gmail.com>
2026-03-18 15:15:01 +0530

    Add files via upload

---
+++ Java-Learning/07.Object Oriented Programming/StudentDemo.java
@@ -0,0 +1,43 @@
+package Introduction;
+
+import java.util.Scanner;
+
+// Student class represents a blueprint (class) for Student objects
+class Student {
+
+    // Instance variables (properties of object)
+    String name;
+    int age;
+
+    // Method to display student details
+    void display() {
+        System.out.println();
+        System.out.println("My name is "+name+" and my age is "+age);
+    }
+}
+
+// Main class (entry point of program)
+public class StudentDemo {
+
+    // main() is the starting point of execution (JVM starts from here)
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Creating an object of Student class. 's1' stores its reference.
+        // 'new Student()' creates object in heap memory
+        Student s1 = new Student();
+
+        System.out.print("Enter the name: ");
+        s1.name = sc.next();
+
+        System.out.print("Enter the age: ");
+        s1.age = sc.nextInt();
+
+        // Display output
+        s1.display();
+
+        // Close scanner
+        sc.close();
+
+    }
+}
