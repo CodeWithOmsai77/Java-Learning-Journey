a2f527bd812fcb8348dba249e26e0238a691bc3e
Omsai Ganesh Dasari <omsai.freelancer7@gmail.com>
2026-03-27 14:13:34 +0530

    Update MethodOverloading.java

--- Java-Learning/07.Object Oriented Programming/MethodOverloading.java
+++ Java-Learning/07.Object Oriented Programming/MethodOverloading.java
@@ -24,3 +24,30 @@ public class MethodOverloading {
         d.add(5.5, 6.5);
     }
 }
+
+
+////
+
+
+class Calculator {
+// Method 1: Add two integers
+int add(int a, int b) {
+return a + b;
+}
+// Method 2: Add three integers
+int add(int a, int b, int c) {
+return a + b + c;
+}
+// Method 3: Add two doubles
+double add(double a, double b) {
+return a + b;
+}
+}
+public class Main {
+public static void main(String[] args) {
+Calculator c = new Calculator();
+System.out.println(c.add(5, 3)); // Output: 8
+System.out.println(c.add(5, 3, 2)); // Output: 10
+System.out.println(c.add(2.5, 3.5)); // Output: 6.0
+}
+}
